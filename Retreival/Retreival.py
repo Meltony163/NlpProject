f9a3dacf1cae6a392ce204e843e2cc7d58510cae
MoamenEltony <moameneltony12@gmail.com>
2026-06-02 14:30:45 +0300

    Retrival added

---
+++ Retreival/Retreival.py
@@ -0,0 +1,65 @@
+from sentence_transformers import SentenceTransformer
+from fastembed import SparseTextEmbedding
+from qdrant_client import QdrantClient
+from qdrant_client.models import SparseVector,Prefetch,FusionQuery,Fusion
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+qdrant_api_key = os.getenv("qdrant_api_key")
+qdrant_url = os.getenv("qdrant_url")
+
+dense_model = SentenceTransformer("all-MiniLM-L6-v2")
+sparse_model = SparseTextEmbedding("Qdrant/bm25")
+
+client = QdrantClient(
+    url=qdrant_url,
+    api_key=qdrant_api_key)
+
+def CheckQuery(query):
+  if isinstance(query, str):
+      return True
+
+  try:
+      return str(query)
+  except :
+      return False
+
+def retreive(query):
+  if not CheckQuery(query):
+    return -1
+
+  query_dense  = dense_model.encode(query).tolist()
+  query_sparse = list(sparse_model.embed([query]))[0]
+
+  results = client.query_points(
+      collection_name="VectorDatabase",
+      prefetch=[
+          Prefetch(query=query_dense, using="dense", limit=10),
+          Prefetch(
+              query=SparseVector(
+                  indices=query_sparse.indices.tolist(),
+                  values=query_sparse.values.tolist(),
+              ),
+              using="sparse",
+              limit=10,
+          ),
+      ],
+      query=FusionQuery(fusion=Fusion.RRF),
+      limit=5,
+      with_payload=True,
+  )
+
+  results_list = []
+  for i in range(5):
+    results_list.append((results.points[i].payload['Context'],results.points[i].payload['Response']))
+
+  return results_list
+
+
+if __name__ == "__main__":
+  user_message = input("User: ")
+  res = retreive(user_message)
+  for message in res:
+    print(message[0])
